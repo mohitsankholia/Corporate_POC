86456123d5c6e57ddc145189323ad0b8f2cc9169
mohitsankholia <sankholia.mohit@gmail.com>
2022-01-19 16:14:20 +0530

    javareact

---
+++ front-crudl/src/App.js
@@ -0,0 +1,77 @@
+import react from 'react';
+/* import List from "./list"
+ import Button from "./Button"
+import Form from "./form" 
+import Carousel from "./carousel"; 
+import Create from "./create"
+import ImageIcon from "./image_icon" */
+
+import List from "./list"
+
+/*
+function App()
+{
+    return(
+        <>
+        <h1> Functional components are new to you guys.</h1>
+        <hr color ='blue'/>
+        <ol style={{backgroundColor:'red'}}>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>BootStrap</li>
+            <li>JS</li>
+        </ol>
+        </>
+    );
+}*/  
+
+/* const vaibhav=()=>{
+    alert("mouse entered")
+}
+
+const kalpana={
+    color:'white',
+    backgroundColor: 'blue'
+}
+
+const App=()=>
+{
+    return(
+        <>
+        <h1 style={kalpana}> Functional components are new to you guys.</h1>
+        <hr color ='blue'/>
+        <ol style={{backgroundColor:'red'}}>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li onMouseEnter={vaibhav}>BootStrap</li>
+            <li>JS</li>
+        </ol>
+        </>
+    );
+}
+
+
+const App=()=>{
+    return(
+        <>
+            <List/>
+            <Form/>
+            <Carousel/>
+            <ImageIcon/>
+        </>
+    );
+}
+*/ 
+
+const App=()=>{
+    return(
+        <>
+        
+            <List/>
+        </>
+    );
+}
+
+
+
+export default App;
---
+++ front-crudl/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react'; // write jsxml content in js file
+import ReactDOM from 'react-dom'; // map interaction with other component
+import App from './App' 
+
+ReactDOM.render(
+  <>
+    <App/>
+     
+  </>
+  ,document.getElementById('root')
+);
+//ReactDOM.render(jsx what to display, on where to display)
+/* ReactDOM.render(
+<>
+<App/> 
+<h1>Welcome to DLithe Bootcamp 2022</h1>
+<h1>Mohit Sankholia</h1>
+<ul>
+  <li>babel</li><li>webpack</li><li>react-dom</li>
+</ul>
+</>
+  ,document.getElementById('root')
+  );
+*/
